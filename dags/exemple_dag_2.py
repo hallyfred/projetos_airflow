5762878bcb7dd4d0e52f19c05ff0b93d001122fa
Frederico Carvalho <64483575+hallyfred@users.noreply.github.com>
2026-09-24 14:11:43 -0300

    criando dags com dependencias

---
+++ dags/exemple_dag_2.py
@@ -0,0 +1,50 @@
+# aqui criaremos um exemplo de dag com quatro tarefas,
+# onde a primeira tarefa será executada primeiro seguida pela segunda e terceira tarefas
+#  e finalmente a quarta tarefa será executada por último.
+
+
+from datetime import datetime
+from time import sleep
+
+from airflow.decorators import dag, task
+
+@dag(
+    dag_id="example_dag",    
+    start_date=datetime(2025, 4, 24),
+    description="A simple DAG with four tasks",
+    schedule="* * * * *",
+    catchup=False #backfill is disabled
+)
+
+def pipeline():
+    @task
+    def first_task():
+        print("First task executed")
+        sleep(2)  # Simulate a delay for the first task
+    @task
+    def second_task():
+        print("Second task executed")
+        sleep(2)  # Simulate a delay for the second task
+
+    @task
+    def third_task():
+        print("Third task executed")
+        sleep(2)  # Simulate a delay for the third task
+    @task
+    def forth_task():
+        print("Forth task executed")
+        sleep(2)  # Simulate a delay for the forth task    
+
+
+    
+    t1 = first_task()
+    t2 = second_task()
+    t3 = third_task()
+    t4 = forth_task()
+
+    # configurando dependencias entre as tarefas
+    # t2 e t3 dependem de t1, e t4 depende de t3
+    t1.set_downstream([t2,t3])
+    t3.set_downstream([t4])
+
+pipeline()
